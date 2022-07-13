91c5974d665eac9f53f3ae1157f2d10d957c4c0c
PopovIR <boovox890@gmail.com>
2022-07-13 16:10:46 +0600

    Вопросы по лабе 16: * Сделаны задания 11,12,13, 14 нужно доделать

--- src/main/java/task14test.java
+++ src/main/java/task14test.java
@@ -8,25 +8,24 @@ public class task14test {
         String a = s.nextLine();
         String[] arr = a.split(",");
         int[] num = new int[arr.length];
+        int[] num2 = new int[num.length + 1];
         int sum = 0;
-        int sum2 = 0;
+
         int val;
-        int val2;
+
         for (int i = 0; i < num.length; i++) {
             num[i] = Integer.parseInt(arr[i]);
         }
         sum = num[0] + num[1];
-        val=num[2];
-        val2 = num[2];
-        num[2] = sum;
 
-        num[3] = val;
-        sum2 = val+ val2;
-        num[4]= sum2;
+
+        num2[2] = sum;
+
         //9,8,7,6,5,4
 
-       for (int y = 0; y < num.length; y++) {
-          System.out.print(num[y]+ " ");}
+        for (int y = 0; y < num.length; y++) {
+            System.out.print(num2[y] + " ");
+        }
 
 
     }
