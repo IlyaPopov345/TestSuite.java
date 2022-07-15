0f2f06c5260d5b336a9976ff10e92f07d4f15eea
PopovIR <boovox890@gmail.com>
2022-07-15 17:05:53 +0600

    Вопросы по лабе 17: * rANGE1

---
+++ src/main/java/testing9020.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.lang.IllegalArgumentException;
+public class testing9020 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        double result = 0;
+
+            result = (Math.sqrt(x + 5) + Math.sqrt(x - 5) )/ ( 2 * Math.sqrt(x));
+            System.out.printf("%.4f", result);}
+
+      //  }
+
+    }
